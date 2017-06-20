685707bba0edc92f796f674cbc3ff321d9e7f78c
PauLeeR <lindaliel@gmail.com>
2017-06-20 01:47:04 -0400

    bici lista, faltan los estilos

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
 var map;
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 16,
+    zoom: 15,
     center: new google.maps.LatLng(-33.91722, 151.23064),
     mapTypeId: 'roadmap'
   });
 
-  var iconBase = "http://maps.google.com/mapfiles/kml/shapes";
+  var iconBase = "http://maps.google.com/mapfiles/kml/shapes";//creo un icono que servirá como marcador del mapa en lugar del habitual
   var icon = {
     bike: {
       icon: iconBase + 'cycling.png'
@@ -42,14 +42,53 @@ function initMap() {
       position: {lat:latitud, lng:longitud},
       animation: google.maps.Animation.DROP,
       map: map, 
-      icon: 'http://maps.google.com/mapfiles/kml/shapes/cycling.png'
+      icon: 'http://icons.iconarchive.com/icons/aha-soft/transport/48/bike-icon.png'//animador/marcador del mapa
     });
 
     map.setZoom(17);
     map.setCenter({lat:latitud, lng:longitud});
-  };
+  }
 
   var notFound = function(error){
     alert("No pudimos encontrar tu ubicación");
   }
-};
+
+  var desde = (document.getElementById("from"));
+  var autocompletar = new google.maps.places.Autocomplete(desde);
+  autocompletar.bindTo("bounds", map);
+  var hacia = (document.getElementById("to"));
+  var autocompletar = new google.maps.places.Autocomplete(hacia);
+  autocompletar.bindTo("bounds", map);
+
+        // ejemplo extraido desde internet
+        //DirectionsService y DirectionsDisplay se desprenden de google, son objetos de google
+        var ds = new google.maps.DirectionsService();//obtiene las coordenadas
+        var dr = new google.maps.DirectionsRenderer();//traduce esas coordenadas a la ruta visible (ruta en azul en el mapa)
+        document.getElementById("from").addEventListener("change", onChangeHandler);
+        document.getElementById("to").addEventListener("change", onChangeHandler);
+        
+        function rutaVisible (ds, dr) {
+          ds.route({
+            origin: document.getElementById("from").value,
+            destination: document.getElementById("to").value,
+            travelMode: "DRIVING"
+          },
+
+          function(response, status) {
+            if (status === "OK") {
+              dr.setDirections(response);
+            } else {
+              window.alert("Ruta "+ status);
+            }
+          });
+        }
+        
+        dr.setMap(map);
+        //onChangeHandler = Agrega una propiedad de seguimiento a una definición de lenguaje específico de dominio
+        var onChangeHandler = function(){
+            //Servicio de indicaciones
+            rutaVisible(ds, dr);
+          }; 
+
+          document.getElementById("route").addEventListener("click",onChangeHandler); 
+}
